6f89acf91b8421c6c06f641a3d80b23e7d63b05c
Matthew West <mattywest@ntlworld.com>
2015-11-22 17:38:30 +0000

    Edited server port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = 8080;
+var serverPort = 3000;
 
 // swaggerRouter configuration
 var options = {
