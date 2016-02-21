41032de317f10d5f33711640e4f5267cd0cc64c6
mrmrtalbot <mrmrtalbot@googlemail.com>
2016-02-21 20:36:34 +0000

    Storm path updates

--- server/models/parcel.js
+++ server/models/parcel.js
@@ -18,6 +18,8 @@ var ParcelSchema = new mongoose.Schema({
 	id: {type: Number, index: true},
 	expiryDate: {type: Date, default: new Date(new Date().setYear(new Date().getFullYear() + 1))},
 	name: {type: String, required: true},
+	currentUser: {type: String, required: true},
+	previousUsers[{type:String}]
 	content: {type: ParcelContentSchema},
 	category: {type: String},
 });
@@ -25,6 +27,7 @@ var ParcelSchema = new mongoose.Schema({
 var ParcelBatchSchema = new mongoose.Schema({
 	dateCreated: {type: Number, default: Date.now()},
 	name: {type: String, required: true},
+	ownerId: { type:String, required: true},
 	passes: {type:Number, min: 0},
 	probability: {type:Number, default: 1, min:0.0001},
 	parcels: [ParcelSchema],
