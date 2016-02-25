f6e65279c1fead93eb03fde003aec66f1af0fe05
Matthew West <mattywest@ntlworld.com>
2016-02-25 19:12:17 +0000

    Index routes setup

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index_routes);
 app.use('/api', api);
 app.use('/api/user', user);
 app.use('/api/provider', provider);
