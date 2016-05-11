71101bfd64bb46c0b4ca0455e24c0f5e39008886
mrmrtalbot <mrmrtalbot@googlemail.com>
2016-05-11 10:16:14 +0100

    adding stormpath to pip

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index_routes);
 app.use('/api', api);
 app.use('/api/parcel', parcel);
